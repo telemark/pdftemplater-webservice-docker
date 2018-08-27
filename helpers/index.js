2818d4a56853bdafb9e398ca1a41733bb99a1a25
Supanut Apikulvanich <supanut.overload@gmail.com>
2018-08-27 23:59:55 +0700

    GCS

--- helpers/index.js
+++ helpers/index.js
@@ -1,4 +1,3 @@
-const fs = require('fs')
 const storage = require('@google-cloud/storage')()
 
 const Config = require('../config')
@@ -7,12 +6,14 @@ const storageBucket = storage.bucket(Config.GCS_STORAGE_BUCKET_NAME)
 const templateBucket = storage.bucket(Config.GCS_TEMPLATE_BUCKET_NAME)
 
 module.exports.uploadToGCS = async (pdfPath) => {
-    await storageBucket.upload(pdfPath, {
+    const result = await storageBucket.upload(pdfPath, {
         gzip: true,
         metadata: {
           cacheControl: 'public, max-age=31536000',
         }
     })
+    console.log(result)
+    return result
 }
 
 module.exports.getTemplatePath = async (templateId) => {
