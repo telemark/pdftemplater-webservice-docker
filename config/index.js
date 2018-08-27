2007a58435ede3c82585dd34732c1081e70846f9
Supanut Apikulvanich <supanut.overload@gmail.com>
2018-08-27 23:54:30 +0700

    GCS

--- config/index.js
+++ config/index.js
@@ -1,7 +1,9 @@
 'use strict'
 
 var config = {
-  SERVER_PORT: process.env.SERVER_PORT || '3000'
+  SERVER_PORT: process.env.SERVER_PORT || '3000',
+  GCS_STORAGE_BUCKET_NAME: process.env.GCS_STORAGE_BUCKET_NAME,
+  GCS_TEMPLATE_BUCKET_NAME: process.env.GCS_TEMPLATE_BUCKET_NAME,
 }
 
 module.exports = config
---
+++ helpers/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+const storage = require('@google-cloud/storage')()
+
+const Config = require('../config')
+
+const templateBucket = storage.bucket(Config.GCS_TEMPLATE_BUCKET_NAME)
+const storageBucket = storage.bucket(Config.GCS_STORAGE_BUCKET_NAME)
+
+module.exports.uploadToGCS = async (pdfPath) => {
+    await storageBucket.upload(pdfPath, {
+        gzip: true,
+        metadata: {
+          cacheControl: 'public, max-age=31536000',
+        }
+    })
+}
+
+module.exports.getTemplatePath = async (templateId) => {
+    const file = templateBucket.file(`templates/${templateId}` + '.docx')
+    const destination = `/tmp/${templateId}.docx`
+    const options = { destination }
+    await file.download(options)
+    return destination
+}
